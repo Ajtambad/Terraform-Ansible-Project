b0b564d0d9770398d2812841e419d8209652cfbe
Ampgh Tambad <amoghtambad@gmail.com>
2025-05-10 13:27:20 -0700

    Working fine, removed all comments from main.tf

--- Terraform_GA/main.tf
+++ Terraform_GA/main.tf
@@ -13,24 +13,12 @@ provider "aws" {
     region = "us-east-1"
 }
 
-# variable "ssh_key_file"{
-#     type = string
-# }
-
-# locals {
-#     user_data_script = <<-EOF
-#             #!/bin/bash
-#             echo ${var.ssh_key_file} >> /home/ec2-user/.ssh/authorized_keys
-#             EOF
-# }
-
 resource "aws_launch_template" "web-server-template" {
     name = "create-web-host"
     image_id = "ami-00a929b66ed6e0de6"
     security_group_names = ["launch-wizard-1"]
     instance_type = "t2.micro"
     key_name = "AWS-key-pair"
-    # user_data = base64encode(local.user_data_script)
 }
 
 resource "aws_autoscaling_group" "web-host-instance" {
